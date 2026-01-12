f67c80772298b138be31983f6229e763de9f76cc
bhaveshbillionaier19 <bhaveshkumarsharmaengineer@gmail.com>
2026-01-12 23:32:21 +0530

    adding files for somnia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,20 @@
 export default {
   solidity: "0.8.20",
   networks: {
-    qie: {
+    // qie testnet
+    // qie: {
+    //   type: "http",
+    //   url: "https://rpc1testnet.qie.digital/",
+    //   chainId: 1983,
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    // },
+    //somnia test net 
+    somniaTestnet: {
       type: "http",
-      url: "https://rpc1testnet.qie.digital/",
-      chainId: 1983,
+      url: "https://dream-rpc.somnia.network/",
+      chainId: 50312,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
+
   },
 };
